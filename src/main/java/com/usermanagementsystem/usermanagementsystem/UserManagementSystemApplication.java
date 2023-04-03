e45d15211b1956cdce258014b3d283d7c85442cf
LucianoFrias <lucianofrias1@hotmail.com>
2023-04-03 15:27:02 -0300

    Compiled Project on WAR

--- src/main/java/com/usermanagementsystem/usermanagementsystem/UserManagementSystemApplication.java
+++ src/main/java/com/usermanagementsystem/usermanagementsystem/UserManagementSystemApplication.java
@@ -3,13 +3,14 @@ package com.usermanagementsystem.usermanagementsystem;
 import com.usermanagementsystem.usermanagementsystem.entities.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @SpringBootApplication
-public class UserManagementSystemApplication {
+public class UserManagementSystemApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(UserManagementSystemApplication.class, args);
 	}
